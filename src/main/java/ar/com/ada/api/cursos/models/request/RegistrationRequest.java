7c2087f72d1c84396cb7b6d3fe758af6ed56bb29
ShadowDrone <shadowdrone@hotmail.com>
2020-09-22 21:36:05 -0300

    Model Validation: se agrega la dependencia Sprinboot starter validation para poder usar notaciones sobre los models y que se validen en el controller. El objeto BindingResult tiene toda la info de como se proceso el JSON al Objeto, dejando una lista de errores. En nuestro caso, se muestra el Register y el Login, de 2 formas diferentes, una modificando el RegistrationResponse agregandole una lista de errores, y en el login devolviendo un ErrorRespone.
    Co-authored-by: AndreaMessina31
    Co-authored-by: laura Izquierdo
    Co-authored-by: AndreaMessina31 <maktub_dharma@hotmail.com>
    Co-authored-by: laura Izquierdo <lauraizquierdo24@gmail.com>
    Co-authored-by: Digneli Dávila <digneli14@gmail.com>

--- src/main/java/ar/com/ada/api/cursos/models/request/RegistrationRequest.java
+++ src/main/java/ar/com/ada/api/cursos/models/request/RegistrationRequest.java
@@ -5,18 +5,23 @@ import java.util.Date;
 import ar.com.ada.api.cursos.entities.Pais.TipoDocuEnum;
 import ar.com.ada.api.cursos.entities.Usuario.TipoUsuarioEnum;
 
+import javax.validation.constraints.*;
+
 /**
  * RegistrationRequest
  */
 public class RegistrationRequest {
 
+    @NotBlank(message = "el nombre no puede ser nulo")
     public String fullName; // Nombre persona
     public Integer country; // pais del usuario
+    @NotNull(message = "El tipo de usuario no puede ser nulo")
     public TipoDocuEnum identificationType; // Tipo Documento
     public String identification; // nro documento
     public Date birthDate; // fechaNacimiento
     public TipoUsuarioEnum userType;
     public String email; // email
+    @NotBlank(message = "La contraseña debe tener al menos 8 digitos")
     public String password; // contraseña elegida por el usuario.
 
 }
--- src/main/java/ar/com/ada/api/cursos/models/response/RegistrationResponse.java
+++ src/main/java/ar/com/ada/api/cursos/models/response/RegistrationResponse.java
@@ -10,4 +10,6 @@ public class RegistrationResponse {
     public boolean isOk = false;
     public String message = "";
     public Integer userId;
+
+    public List<ErrorResponseItem> errors = new ArrayList<>();
 }
